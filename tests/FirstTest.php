5da23b4efe40a7a4e725ab22af13bfe5f16c428c
helga <o.alekseenko@leboutique.com>
2015-11-17 20:39:39 +0200

    min and max

--- tests/FirstTest.php
+++ tests/FirstTest.php
@@ -6,5 +6,24 @@ class FirstTest extends \PHPUnit_Framework_TestCase
     {
         $this->assertEquals(1, 1);
         $this->assertEquals(1 + 1, 2);
+
+
+        $this->assertTrue(true);
+        $this->assertTrue(1 < 2);
+        $this->assertTrue(1 == 1);
+        $this->assertTrue(1 == '1');
+
+
+        $this->assertTrue(1 === 1);
+        $this->assertTrue(100000 === 100000);
+        $this->assertSame(1, 1);
+        $this->assertFalse(1 === '1');
+        $this->assertNotSame(1, '1');
+    }
+
+    public function testMax()
+    {
+        $this->assertTrue(max(1, 2, 3) === 3);
+        $this->assertTrue(min(1, 2, 3) === 1);
     }
 }
